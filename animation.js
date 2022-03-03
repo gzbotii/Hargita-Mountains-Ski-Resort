98e120e4ba1ca6f29ad0fa276a0eeeec7d35898e
gzbotii <gzbotii@gmail.com>
2022-03-03 14:28:27 +0200

    upadting animation

--- animation.js
+++ animation.js
@@ -1,14 +1,13 @@
-// const { default: gsap } = require("gsap/gsap-core");
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".footer-col", {
   scrollTrigger: {
     trigger: ".footer",
   },
-  duration: 0.7,
+  duration: 0.6,
   ease: "slow(0.7, 0.7, false)",
   y: 70,
-  delay: 0.2,
+  delay: 0.3,
 });
 
 gsap.from("header", {
@@ -28,5 +27,34 @@ gsap.from(".nav-link, .weather-info, weather-icon", {
 gsap.from(".logo", {
   duration: 1,
   opacity: 0,
-  delay: 1.5,
+  delay: 1.2,
+});
+
+gsap.from("h3, .carousel-item p", {
+  scrollTrigger: {
+    trigger: "h3",
+  },
+  duration: 0.7,
+  ease: "slow(0.7, 0.7, false)",
+  y: 70,
+});
+
+gsap.from(".arrow-prev", {
+  scrollTrigger: {
+    trigger: "p.arrow",
+  },
+  duration: 0.7,
+  ease: "elastic.out(1.5, 0.3)",
+  x: 20,
+  delay: 1,
+});
+
+gsap.from(".arrow-next", {
+  scrollTrigger: {
+    trigger: "arrow",
+  },
+  duration: 0.7,
+  ease: "elastic.out(1.5, 0.3)",
+  x: -20,
+  delay: 1,
 });
