14c0ed7bcbb0f4c9fd2fb1788b10f44dc7d765cb
gzbotii <gzbotii@gmail.com>
2022-03-03 19:48:36 +0200

    map set-up

---
+++ map.js
@@ -0,0 +1,11 @@
+function initMap() {
+  const hr = { lat: 46.390857, lng: 25.634466 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: hr,
+  });
+  const marker = new google.maps.Marker({
+    position: hr,
+    map: map,
+  });
+}
