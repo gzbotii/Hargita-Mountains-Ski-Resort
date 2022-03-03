8d48dc548ba25ea329a216f37562d33b9c151e46
gzbotii <gzbotii@gmail.com>
2022-03-03 13:51:48 +0200

    some nice animation

--- animation.js
+++ animation.js
@@ -1,17 +1,32 @@
-gsap.from("header", { duration: 0.4, ease: "back.out(1.2)", y: -500 });
+// const { default: gsap } = require("gsap/gsap-core");
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".footer-col", {
+  scrollTrigger: {
+    trigger: ".footer",
+  },
+  duration: 0.7,
+  ease: "slow(0.7, 0.7, false)",
+  y: 70,
+  delay: 0.2,
+});
+
+gsap.from("header", {
+  duration: 0.4,
+  ease: "back.out(1.1)",
+  y: -500,
+});
 
 gsap.from(".nav-link, .weather-info, weather-icon", {
   duration: 0.4,
   ease: "back.outexpo.out",
-  y: -900,
+  y: -100,
   delay: 0.4,
   stagger: 0.1,
 });
 
-// gsap.from("#temp, #description", {
-//   duration: 0.4,
-//   ease: "back.outexpo.out",
-//   x: +200,
-//   delay: 0.8,
-//   stagger: 0.1,
-// });
+gsap.from(".logo", {
+  duration: 1,
+  opacity: 0,
+  delay: 1.5,
+});
