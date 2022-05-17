41a67e90bf29d0c312fa17e6e95f122e59e01ac3
gzbotii <gzbotii@gmail.com>
2022-05-17 09:05:23 +0300

    Publishing

--- mobile-nav.js
+++ mobile-nav.js
@@ -1,5 +1,16 @@
 const navMobile = document.getElementById("nav_mobile");
+const navMobileChildItem = document.querySelectorAll(".nav-mobile-child-item");
 
 function hamburgr() {
   navMobile.classList.toggle("nav_mobile_open");
+  }
+
+function hamburgrClosed() {
+  navMobileChildItem.forEach((item) => {
+    // console.log(item);
+    item.addEventListener("click", function() {
+      console.log('click')
+      
+    })
+  }) 
 }
