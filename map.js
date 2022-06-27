2d17c38fee5185ff8ac0711b8a44b4ef24b62cb6
gzbotii <gzbotii@gmail.com>
2022-06-27 08:48:33 +0300

    a newer version for the map

--- map.js
+++ map.js
@@ -1,3 +1,17 @@
+// function initMap() {
+//   const hr = { lat: 46.385066, lng: 25.659792 };
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 15,
+//     center: hr,
+//   });
+//   const marker = new google.maps.Marker({
+//     position: hr,
+//     map: map,
+//   });
+// }
+
+// window.onload = initMap();
+
 function initMap() {
   const hr = { lat: 46.385066, lng: 25.659792 };
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -10,4 +24,4 @@ function initMap() {
   });
 }
 
-window.onload = initMap();
+window.initMap = initMap;
