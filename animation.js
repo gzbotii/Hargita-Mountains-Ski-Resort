bf903a7ae4bde346023d9d1295d867a8bf49b248
gzbotii <gzbotii@gmail.com>
2022-09-07 11:11:38 +0300

    map moved to animation.js

--- animation.js
+++ animation.js
@@ -115,6 +115,18 @@ function animation() {
 
 // window.onload = animation();
 
+function initMap() {
+  const hr = { lat: 46.385066, lng: 25.659792 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: hr,
+  });
+  const marker = new google.maps.Marker({
+    position: hr,
+    map: map,
+  });
+}
+
 window.onload = () => {
-  animation();
+  animation(), initMap();
 };
--- map.js
+++ map.js
@@ -1,15 +1,15 @@
-function initMap() {
-  const hr = { lat: 46.385066, lng: 25.659792 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
-    center: hr,
-  });
-  const marker = new google.maps.Marker({
-    position: hr,
-    map: map,
-  });
-}
+// function initMap() {
+//   const hr = { lat: 46.385066, lng: 25.659792 };
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 15,
+//     center: hr,
+//   });
+//   const marker = new google.maps.Marker({
+//     position: hr,
+//     map: map,
+//   });
+// }
 
-//window.onload = initMap();
+// //window.onload = initMap();
 
-window.initMap = initMap;
+// //window.initMap = initMap;
