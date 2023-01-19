659d32efb5dbe212fdb88a42dd9aa8c366b0f662
gzbotii <gzbotii@gmail.com>
2023-01-19 08:04:52 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -7,9 +7,18 @@ function hamburgr() {
 // Add an event listener to the navMobile element that listens for a click event
 navMobile.addEventListener("click", hamburgr);
 
-const navigationHeight = document.querySelector("nav").offsetHeight;
-
-document.documentElement.style.setProperty(
-  "--scroll-padding",
-  navigationHeight - 1 + "px"
-);
+if (window.innerWidth < 1080) {
+  const topElementsHeight = document.querySelector("top-elements").offsetHeight;
+  const weatherHeight = document.querySelector("weather").offsetHeight;
+  const totalHeight = topElementsHeight + weatherHeight;
+  document.documentElement.style.setProperty(
+    "--scroll-padding",
+    totalHeight - 1 + "px"
+  );
+} else {
+  const navigationHeight = document.querySelector("nav").offsetHeight;
+  document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navigationHeight - 1 + "px"
+  );
+}
